b0e2d76ec1ebb72193bdcaa9ef7798fd6e8ee86a
Samuel Ribeiro souza <samuelribeiro03181620@gmail.com>
2026-08-26 11:41:01 +0000

    999

---
+++ 3-bimestre/orientacao-a-objetos/aula12/index.js
@@ -0,0 +1,22 @@
+class Usuario {
+    static quantidade = 0
+      
+    constructor(nome) {
+        this.nome = nome    
+        Usuario.quantidade++ 
+    }
+
+    static quantidadeUsuarios() {
+        return Usuario.quantidade 
+    }
+
+}
+
+const usuario1 = new Usuario("Ana")
+const usuario2 = new Usuario("Carlos")
+const usuario3 = new Usuario("Maria")
+
+console.log(Usuario.quantidadeUsuarios()) 
+
+console.log(usuario1.nome)
+
