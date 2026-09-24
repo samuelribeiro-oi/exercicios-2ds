f2634b792d4436be1914390466390fb81018e8df
Samuel Ribeiro souza <samuelribeiro03181620@gmail.com>
2026-09-24 13:03:05 -0300

    Add files via upload

--- 3-bimestre/orientacao-a-objetos/aula16/index.js
+++ 3-bimestre/orientacao-a-objetos/aula16/index.js
@@ -6,8 +6,12 @@ class Veiculo {
         this.modelo = modelo
     }
 
+    apresentar() {
+        return `${this.marca} ${this.modelo}`
+    }
+
     descricao() {
-        return `Marca: ${this.marca} | Modelo: ${this.modelo}`
+        return this.apresentar()
     }
 
 }
@@ -18,10 +22,6 @@ class Carro extends Veiculo {
         super(marca, modelo)
 
         this.portas = portas
-    }
-
-    descricao() {
-        return `${super.descricao()} | Portas: ${this.portas}`
     }
 
     abrirPortas() {
@@ -38,17 +38,11 @@ class Moto extends Veiculo {
         this.cilindrada = cilindrada
     }
 
-    descricao() {
-        return `${super.descricao()} | Cilindrada: ${this.cilindrada}cc`
-    }
-
     ligarMotor() {
         return "O motor da moto foi ligado"
     }
 
 }
-
-
 
 module.exports = {
     Veiculo,
@@ -56,19 +50,17 @@ module.exports = {
     Moto
 }
 
-
-
 if (require.main === module) {
 
     const carro = new Carro("Toyota", "Corolla", 4)
     const moto = new Moto("Honda", "CG 160", 160)
 
     console.log("=== CARRO ===")
-    console.log(carro.descricao())
+    console.log(carro.apresentar())
     console.log(carro.abrirPortas())
 
     console.log("\n=== MOTO ===")
-    console.log(moto.descricao())
+    console.log(moto.apresentar())
     console.log(moto.ligarMotor())
 
 }
